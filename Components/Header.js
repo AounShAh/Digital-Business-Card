504484f081309f3b4aa9eb06ad68bfcc21234820
Unknown Person <102745850+AounShAh@users.noreply.github.com>
2023-07-01 09:14:39 +0500

    intial commit, unfinished
    there are some errors in it

---
+++ Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function Header()
+{
+    return (
+        <div className='header'>
+            <h3>Aoun Shah</h3>
+            <h4>Frontend Developer</h4>
+            <h5>aounshah.website</h5>
+            <button id='email' className='emailBtn'>Email</button>
+        </div>
+    )
+}
---
+++ Components/Interests.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Interests()
+{
+    return (
+        <div className='Interests'>
+            <h1>interests</h1>
+            <p>Cyber security ,Networking , Pentesting, lorem</p>
+        </div>
+    )
+}
